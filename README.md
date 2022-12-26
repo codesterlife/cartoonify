1aabeb2b0f70563727ac88c0f9d33733ac0e287c
codesterlife <codesterlife@gmail.com>
2022-12-27 02:21:58 +0530

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,29 @@
+# Cartoonify
+
+##### Cartoonify images using OpenCV on Python.
+
+#### Implementation Details
+
+This Project is built using VSCode. Initial it was meant to be created in the form of a jupyter notebook but I ran into some issues with that. So I opted to just create it as a normal python file.
+
+I personally used venv as my virtual environment for dependency managements, although It not absolutely to do so. I've been really finding using virtual envs productive for my projects.
+
+I have provided the requirements.txt file incase anyone want to try it out.
+
+#### Steps to run the program
+
+##### Using venv:
+* `python3 -m venv NAME`  to create a virtual env inside the cloned repo
+
+* `source NAME/bin/activate`  to activate the virtual env
+* `pip install -r requirements.txt`  to install the required dependencies
+
+And your are pretty much good to go!
+
+##### Running Normally:
+
+* Just run the `pip install -r requirements.txt` command before u try to run the program.
+
+__I dont think every package/module mentioned in the requirements.txt is absolutely neccessary to run the program. I have removed ones I'm absolutely sure the it won't need.__
+
+There is a version of this program with a GUI that I'm working on. But it still has some errors that need to be fixed.
---
+++ cartoonify.py
@@ -0,0 +1,89 @@
+import cv2 as cv
+import numpy as np
+import easygui
+import imageio
+import sys
+import matplotlib.pyplot as plt
+import sys
+import os
+
+def upload():
+
+    """ fileopenbox opens the box to choose file and choose file and help us store file path as string """
+    
+    ImagePath = easygui.fileopenbox()
+    cartoonify(ImagePath)
+
+def cartoonify(ImagePath):
+
+    originalImage = cv.imread(ImagePath)
+
+    # check if image was not chosen
+
+    if originalImage is None:
+        print("Cannot find any image. Choose appropriate file!")
+        sys.exit()
+
+    # read the image
+
+    originalImage = cv.cvtColor(originalImage, cv.COLOR_BGR2RGB)
+    resized1 = cv.resize(originalImage, (378, 496))
+
+    # plt.imshow(resized1, cmap='gray')
+
+    """ Multiple transfromaations have to be done on a image to make it into a cartoon like image. First transformation is into grayscale."""
+
+    # converting an image into grayscale
+
+    grayScaleImage = cv.cvtColor(originalImage, cv.COLOR_BGR2GRAY)
+    resized2 = cv.resize(grayScaleImage, (378, 496))
+
+    # plt.imshow(resized2, cmap='gray')
+
+    """ Next transformation is to smooth the grayscale image """
+
+    smoothGrayScale = cv.medianBlur(grayScaleImage, 5)
+    resized3 = cv.resize(smoothGrayScale, (378, 496))
+
+    # plt.imshow(resized3, cmap='gray')
+
+    """ Next transformation is to retrieve the edges of the image """
+
+    # retreiving the edges for cartoon effect by using the thresholding technique
+
+    getEdge = cv.adaptiveThreshold(
+        smoothGrayScale, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 9, 9)
+
+    resized4 = cv.resize(getEdge, (378, 496))
+
+    # plt.imshow(resized4, cmap='gray')
+
+    """ Next transformation is to prepare a mask image """
+
+    # applying bilateral filter to remove noise and keep edge sharp as required
+
+    colorImage = cv.bilateralFilter(originalImage, 9, 9, 300, 300)
+    resized5 = cv.resize(colorImage, (378, 496))
+
+    # plt.imshow(resized5, cmap='gray')
+
+    """ Next transformation is a give a cartoon effect """
+
+    # masking edged image with our beautified image
+
+    cartoonImage = cv.bitwise_and(colorImage, colorImage, mask = getEdge)
+    resized6 = cv.resize(cartoonImage, (378, 496))
+
+    # plt.imshow(resized6, cmap='gray')
+
+    """ Plotting all the transformations """
+    images = [resized1, resized2, resized3, resized4, resized5, resized6]
+
+    fig, axes = plt.subplots(2, 3, figsize=(8, 8), subplot_kw={'xticks': [], 'yticks': []}, gridspec_kw=dict(hspace=0.1, wspace=0.1))
+
+    for i, ax in enumerate(axes.flat):
+        ax.imshow(images[i], cmap='gray')
+
+    plt.show()
+
+upload()
